9a57033e3d5e4ecbe1a4b1ef946e0dc3db77e579
AzerUI1 <azer0808aslanov@gmail.com>
2025-10-04 13:02:20 +0400

    C

--- main.h
+++ main.h
@@ -1,10 +1,141 @@
-#ifndef MAIN_H
-#define MAIN_H
+#include "main.h"
 
-extern char **environ;
+/**
+ * command_exists - checks if a command exists in PATH or is absolute
+ * @cmd: command string
+ * Return: malloc'd full path if exists, else NULL
+ */
+char *command_exists(char *cmd)
+{
+    char *path_env, *path_copy, *token, *full_path;
+    struct stat st;
 
-void execute_command(char *line);
-char *command_exists(char *cmd);
+    if (!cmd)
+        return NULL;
 
-#endif /* MAIN_H */
+    if (strchr(cmd, '/')) /* Absolute or relative path */
+    {
+        if (stat(cmd, &st) == 0 && (st.st_mode & S_IXUSR))
+            return strdup(cmd);
+        return NULL;
+    }
+
+    path_env = getenv("PATH");
+    if (!path_env)
+        return NULL;
+
+    path_copy = strdup(path_env);
+    if (!path_copy)
+        return NULL;
+
+    token = strtok(path_copy, ":");
+    while (token)
+    {
+        full_path = malloc(strlen(token) + strlen(cmd) + 2);
+        if (!full_path)
+        {
+            free(path_copy);
+            return NULL;
+        }
+        sprintf(full_path, "%s/%s", token, cmd);
+
+        if (stat(full_path, &st) == 0 && (st.st_mode & S_IXUSR))
+        {
+            free(path_copy);
+            return full_path;
+        }
+
+        free(full_path);
+        token = strtok(NULL, ":");
+    }
+
+    free(path_copy);
+    return NULL;
+}
+
+/**
+ * execute_command - executes a single command line
+ * @line: command line string
+ */
+void execute_command(char *line)
+{
+    pid_t pid;
+    int status;
+    char *argv[64];
+    int i = 0;
+    char *token;
+    char *full_cmd;
+
+    /* Split command line into arguments */
+    token = strtok(line, " \t\n");
+    while (token && i < 63)
+    {
+        argv[i++] = token;
+        token = strtok(NULL, " \t\n");
+    }
+    argv[i] = NULL;
+
+    if (argv[0] == NULL) /* Empty command */
+        return;
+
+    full_cmd = command_exists(argv[0]);
+    if (!full_cmd)
+    {
+        fprintf(stderr, "./shell: %s: not found\n", argv[0]);
+        return;
+    }
+
+    pid = fork();
+    if (pid == -1)
+    {
+        perror("fork");
+        free(full_cmd);
+        return;
+    }
+
+    if (pid == 0) /* Child process */
+    {
+        if (execve(full_cmd, argv, environ) == -1)
+        {
+            perror("./shell");
+            free(full_cmd);
+            exit(EXIT_FAILURE);
+        }
+    }
+    else /* Parent process */
+        wait(&status);
+
+    free(full_cmd);
+}
+
+/**
+ * main - simple shell main loop
+ * Return: 0
+ */
+int main(void)
+{
+    char *line = NULL;
+    size_t len = 0;
+    ssize_t nread;
+
+    while (1)
+    {
+        printf("$ ");
+        nread = getline(&line, &len, stdin);
+        if (nread == -1) /* EOF or error */
+        {
+            putchar('\n');
+            break;
+        }
+
+        /* Remove trailing newline */
+        if (line[nread - 1] == '\n')
+            line[nread - 1] = '\0';
+
+        execute_command(line);
+    }
+
+    free(line);
+    return 0;
+}
 
--- shell.c
+++ shell.c
@@ -1,162 +1,141 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <unistd.h>
-#include <sys/wait.h>
-#include <sys/stat.h>
 #include "main.h"
 
 /**
- * command_exists - checks if command exists in PATH or as absolute path
- * @cmd: command to check
- *
- * Return: full path if exists (malloc), NULL otherwise
+ * command_exists - checks if a command exists in PATH or is absolute
+ * @cmd: command string
+ * Return: malloc'd full path if exists, else NULL
  */
 char *command_exists(char *cmd)
 {
-	char *path_env, *path_copy, *token, *full_path;
-	struct stat st;
+    char *path_env, *path_copy, *token, *full_path;
+    struct stat st;
 
-	if (!cmd)
-		return (NULL);
+    if (!cmd)
+        return NULL;
 
-	if (strchr(cmd, '/'))
-	{
-		if (stat(cmd, &st) == 0 && (st.st_mode & S_IXUSR))
-			return strdup(cmd);
-		return NULL;
-	}
+    if (strchr(cmd, '/')) /* Absolute or relative path */
+    {
+        if (stat(cmd, &st) == 0 && (st.st_mode & S_IXUSR))
+            return strdup(cmd);
+        return NULL;
+    }
 
-	path_env = getenv("PATH");
-	if (!path_env)
-		return NULL;
+    path_env = getenv("PATH");
+    if (!path_env)
+        return NULL;
 
-	path_copy = strdup(path_env);
-	if (!path_copy)
-		return NULL;
+    path_copy = strdup(path_env);
+    if (!path_copy)
+        return NULL;
 
-	token = strtok(path_copy, ":");
-	while (token)
-	{
-		full_path = malloc(strlen(token) + strlen(cmd) + 2);
-		if (!full_path)
-		{
-			free(path_copy);
-			return NULL;
-		}
-		sprintf(full_path, "%s/%s", token, cmd);
-		if (stat(full_path, &st) == 0 && (st.st_mode & S_IXUSR))
-		{
-			free(path_copy);
-			return full_path;
-		}
-		free(full_path);
-		token = strtok(NULL, ":");
-	}
-	free(path_copy);
-	return NULL;
+    token = strtok(path_copy, ":");
+    while (token)
+    {
+        full_path = malloc(strlen(token) + strlen(cmd) + 2);
+        if (!full_path)
+        {
+            free(path_copy);
+            return NULL;
+        }
+        sprintf(full_path, "%s/%s", token, cmd);
+
+        if (stat(full_path, &st) == 0 && (st.st_mode & S_IXUSR))
+        {
+            free(path_copy);
+            return full_path;
+        }
+
+        free(full_path);
+        token = strtok(NULL, ":");
+    }
+
+    free(path_copy);
+    return NULL;
 }
 
 /**
- * execute_command - forks and executes a command with arguments
+ * execute_command - executes a single command line
  * @line: command line string
  */
 void execute_command(char *line)
 {
-	pid_t pid;
-	int status;
-	char *argv[64];
-	int i = 0;
-	char *token;
-	char *full_cmd;
+    pid_t pid;
+    int status;
+    char *argv[64];
+    int i = 0;
+    char *token;
+    char *full_cmd;
 
-	token = strtok(line, " \t");
-	while (token && i < 63)
-	{
-		argv[i++] = token;
-		token = strtok(NULL, " \t");
-	}
-	argv[i] = NULL;
+    /* Split command line into arguments */
+    token = strtok(line, " \t\n");
+    while (token && i < 63)
+    {
+        argv[i++] = token;
+        token = strtok(NULL, " \t\n");
+    }
+    argv[i] = NULL;
 
-	full_cmd = command_exists(argv[0]);
-	if (!full_cmd)
-	{
-		fprintf(stderr, "./shell: %s: not found\n", argv[0]);
-		return;
-	}
+    if (argv[0] == NULL) /* Empty command */
+        return;
 
-	pid = fork();
-	if (pid == -1)
-	{
-		perror("fork");
-		free(full_cmd);
-		return;
-	}
+    full_cmd = command_exists(argv[0]);
+    if (!full_cmd)
+    {
+        fprintf(stderr, "./shell: %s: not found\n", argv[0]);
+        return;
+    }
 
-	if (pid == 0)
-	{
-		if (execve(full_cmd, argv, environ) == -1)
-		{
-			perror("./shell");
-			exit(EXIT_FAILURE);
-		}
-	}
-	else
-		wait(&status);
+    pid = fork();
+    if (pid == -1)
+    {
+        perror("fork");
+        free(full_cmd);
+        return;
+    }
 
-	free(full_cmd);
+    if (pid == 0) /* Child process */
+    {
+        if (execve(full_cmd, argv, environ) == -1)
+        {
+            perror("./shell");
+            free(full_cmd);
+            exit(EXIT_FAILURE);
+        }
+    }
+    else /* Parent process */
+        wait(&status);
+
+    free(full_cmd);
 }
 
 /**
- * main - simple UNIX shell with PATH and arguments
- *
- * Return: Always 0
+ * main - simple shell main loop
+ * Return: 0
  */
 int main(void)
 {
-	char *line = NULL;
-	size_t len = 0;
-	ssize_t nread;
-	char *command;
-	char *end;
+    char *line = NULL;
+    size_t len = 0;
+    ssize_t nread;
 
-	while (1)
-	{
-		if (isatty(STDIN_FILENO))
-		{
-			printf(":) ");
-			fflush(stdout);
-		}
+    while (1)
+    {
+        printf("$ ");
+        nread = getline(&line, &len, stdin);
+        if (nread == -1) /* EOF or error */
+        {
+            putchar('\n');
+            break;
+        }
 
-		nread = getline(&line, &len, stdin);
-		if (nread == -1)
-		{
-			if (isatty(STDIN_FILENO))
-				printf("\n");
-			break;
-		}
+        /* Remove trailing newline */
+        if (line[nread - 1] == '\n')
+            line[nread - 1] = '\0';
 
-		command = strtok(line, "\n");
-		while (command)
-		{
-			while (*command == ' ' || *command == '\t')
-				command++;
+        execute_command(line);
+    }
 
-			end = command + strlen(command) - 1;
-			while (end > command && (*end == ' ' || *end == '\t'))
-			{
-				*end = '\0';
-				end--;
-			}
-
-			if (*command != '\0')
-				execute_command(command);
-
-			command = strtok(NULL, "\n");
-		}
-	}
-
-	free(line);
-	return (0);
+    free(line);
+    return 0;
 }
 
